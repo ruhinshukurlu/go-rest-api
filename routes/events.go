f454aad6d0ebdff380578cc27e882abb6977ead0
ruhinshukurlu <ruhinshukurlu@gmail.com>
2025-09-08 16:52:41 +0400

    delete route added

--- routes/events.go
+++ routes/events.go
@@ -93,3 +93,27 @@ func updateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "updated"})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Some error..."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Some error..."})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldn't delete..."})
+		return
+	}
+
+	context.JSON(http.StatusNoContent, gin.H{"message": "Successs deleted"})
+}
