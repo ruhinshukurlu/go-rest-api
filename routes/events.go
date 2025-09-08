c7d7270f97f1491fd9df73519e95a158c2ef323a
ruhinshukurlu <ruhinshukurlu@gmail.com>
2025-09-08 14:25:57 +0400

    update event route added

--- routes/events.go
+++ routes/events.go
@@ -59,3 +59,37 @@ func createEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Created Successfully!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Some error..."})
+		return
+	}
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Some error..."})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Some error..."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Some error..."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "updated"})
+}
