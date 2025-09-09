95cafd2f5515c0577154122eee1a89777b82f09e
ruhinshukurlu <ruhinshukurlu@gmail.com>
2025-09-09 10:37:17 +0400

    password hashing

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/go-rest-api/database"
+import (
+	"github.com/go-rest-api/database"
+	"github.com/go-rest-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -19,7 +22,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPass, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPass)
 
 	if err != nil {
 		return err
